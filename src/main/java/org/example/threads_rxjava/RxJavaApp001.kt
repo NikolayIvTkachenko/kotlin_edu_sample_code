4d87e565ce53ba1ef1f059b92799398bbc15fbfa
NikolayTkachenko <tkachenni@mail.ru>
2026-01-28 03:32:23 +0300

    Udate code 0

--- src/main/java/org/example/threads_rxjava/RxJavaApp001.kt
+++ src/main/java/org/example/threads_rxjava/RxJavaApp001.kt
@@ -1,16 +1,34 @@
 package org.example.threads_rxjava
 
 
-
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.*
 
 fun main() {
-
+    programm01_rxjava()
 }
 
 fun programm01_rxjava() {
     var list01: List<Any> = listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f)
-   // var observable: Observable<Any> = list01.toObservable()
+    var observable: Observable<Any> = list01.toObservable()
 
-    // observable.subscribeBy
+     observable.subscribeBy(
+         onNext = {
+             println(it)
+         },
+         onError = {
+             it.printStackTrace()
+         },
+         onComplete = {
+             println("Done")
+         }
+     )
+
+}
+
+
+fun programm02_rxjava() {
+   //var subject: Subject<int>
+
 
 }
