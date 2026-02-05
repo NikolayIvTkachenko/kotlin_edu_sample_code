ba954ce607231cfbcb8cb0235274118fb8597588
NikolayTkachenko <tkachenni@mail.ru>
2026-02-06 01:11:32 +0300

    Udate code 0

--- src/main/java/org/example/algoriyhms_search_sort/0_CountTimeComplexityApp.kt
+++ src/main/java/org/example/algoriyhms_search_sort/0_CountTimeComplexityApp.kt
@@ -1,5 +1,62 @@
 package org.example.algoriyhms_search_sort
 
 fun main() {
+    programTimeTracker()
 
+
+}
+
+fun programTimeTracker() {
+    val startTime = System.currentTimeMillis().toLong()
+
+    // program001()
+    // program002()
+    program003()
+
+
+    val stopTime = System.currentTimeMillis().toLong()
+    val elapsedTime: Long = stopTime - startTime
+
+    println("startTime = $startTime")
+    println("stopTime = $stopTime")
+    println("elapsedTime = $elapsedTime")
+}
+
+fun program001() {
+    print("Enter an integer : ")
+    val ndata = readlnOrNull()?.toInt() ?: 0
+    var a = ndata / 100
+    var b = (ndata / 10) % 10
+    var c = ndata % 10
+    var revert = c * 100 + b * 10 + a
+    println("a = $a")
+    println("b = $b")
+    println("c = $c")
+    println("revert = $revert")
+}
+
+fun program002() {
+    print("Enter an integer : ")
+    val ndata = readlnOrNull()?.toInt() ?: 0
+    var count = 0
+    for (i in 1..ndata) {
+        print("Enter number : ")
+        val ninput = readlnOrNull()?.toInt() ?: 0
+        if (ninput % 10 == 0) {
+            count += 1
+        }
+    }
+    println("count = $count")
+}
+
+fun program003() {
+    print("Enter an integer : ")
+    val ndata = readlnOrNull()?.toInt() ?: 0
+    var count = 0
+    var i = 1
+    while (i <= ndata * ndata) {
+        count++
+        i *= 3
+    }
+    println("count = $count")
 }
--- src/main/java/org/example/algoriyhms_search_sort/1_LinearSearchApp.kt
+++ src/main/java/org/example/algoriyhms_search_sort/1_LinearSearchApp.kt
@@ -2,4 +2,89 @@ package org.example.algoriyhms_search_sort
 
 fun main() {
 
+    programTimeTrackerLinearSearch()
+
+}
+
+fun programTimeTrackerLinearSearch() {
+    val startTime = System.currentTimeMillis().toLong()
+
+    // programLinearSearch001()
+    // programLinearSearch002()
+    programLinearSearch003()
+
+
+    val stopTime = System.currentTimeMillis().toLong()
+    val elapsedTime: Long = stopTime - startTime
+
+    println("startTime = $startTime")
+    println("stopTime = $stopTime")
+    println("elapsedTime = $elapsedTime")
+}
+
+fun programLinearSearch001() {
+    val numbers = arrayOf(8, 11, 9, 6, 10, 7, 2, 3, 1, 4, 5)
+    val key = 2
+    println("numbers = $numbers")
+    println("key = $key")
+    val result = LinearySearch(numbers, key)
+
+    println("result = $result")
+}
+
+fun LinearySearch(numbers: Array<Int>, key: Int): Boolean {
+    println("Lineary Search")
+    for (i in numbers.indices) {
+        if (numbers[i] == key) return true
+    }
+    return false
+}
+
+fun programLinearSearch002() {
+    println("Binary Search")
+    val numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+    val key = 8
+    val resultIndex = BinarySearch(numbers, key)
+    println("resultIndex = $resultIndex")
+    println("numbers[resultIndex] = ${numbers[resultIndex]}")
+}
+
+fun BinarySearch(numbers: Array<Int>, key: Int): Int {
+    println("BinarySearch")
+    var left = 0
+    var right = numbers.size - 1
+    println("key = $key")
+    println("left = $left")
+    println("right = $right")
+
+    while (left <= right) {
+        var m = left + (right - left) / 2
+        if (numbers[m] == key) return m
+        if (numbers[m] < key) left = m + 1
+        else right = m - 1
+    }
+    return -1
+}
+
+fun programLinearSearch003() {
+    val numbers = arrayOf(8, 11, 9, 6, 10, 7, 2, 3, 1, 4, 5)
+    val key = 2
+    println("numbers = $numbers")
+    println("key = $key")
+    val result = LeftBinarySearch(numbers, key)
+    println("result = $result")
+}
+
+fun LeftBinarySearch(numbers: Array<Int>, key: Int): Int {
+    println("LeftBinarySearch")
+    var left = -1
+    var right = numbers.size - 1
+
+    while (left + 1 < right) {
+        var m = left + (right - left) / 2
+        if (numbers[m] < key) left = m
+        else right = m
+    }
+    if (numbers[right] == key) return  right
+    return  -1
 }
